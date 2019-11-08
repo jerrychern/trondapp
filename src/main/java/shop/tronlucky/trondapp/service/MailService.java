5151bb675137fc1bcd15ae869f910613353942aa
jerrychern <chenwangjie190422@credithc.com>
2019-11-08 14:59:17 +0800

    config email sender and recipients.

--- src/main/java/shop/tronlucky/trondapp/service/MailService.java
+++ src/main/java/shop/tronlucky/trondapp/service/MailService.java
@@ -1,8 +1,8 @@
 package shop.tronlucky.trondapp.service;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +10,23 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class MailService {
-    @Autowired
-    JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String sender;
 
-    public void sendMail(String subject, String msg){
+    @Value("${spring.mail.recipients}")
+    private String[] recipients;
+
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String subject, String msg) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("cwangjie@qq.com");
-        message.setTo("cwangjie@qq.com");
+        message.setFrom(sender);
+        message.setTo(recipients);
         message.setSubject(subject);
         message.setText(msg);
         mailSender.send(message);

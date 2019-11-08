bea5bed71f827bac00e58ff6f013495c159a3876
jerrychern <chenwangjie190422@credithc.com>
2019-11-08 23:05:21 +0800

    config email sender and recipients.

--- src/main/resources/DB/DDL_SQL.sql
+++ src/main/resources/DB/DDL_SQL.sql
@@ -1,13 +1,11 @@
-DROP TABLE IF EXISTS `withdraw_fail_log`;
-CREATE TABLE `withdraw_fail_log` (
+CREATE TABLE IF NOT EXISTS `withdraw_fail_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增',
   `round_number` int(50) DEFAULT NULL COMMENT '轮次号',
   `withdraw_type` int(2) DEFAULT NULL COMMENT '提现类型: 1 普通提现; 2 大奖提现',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='提现失败日志表';
 
-DROP TABLE IF EXISTS `secret`;
-CREATE TABLE `secret` (
+CREATE TABLE IF NOT EXISTS `secret` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增',
   `round_number` int(50) DEFAULT NULL COMMENT '轮次号,唯一索引',
   `key` varchar(256) DEFAULT NULL COMMENT '原始秘钥',
